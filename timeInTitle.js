21e2f0a58402117e161b43f42a8ab207974d9260
waka5791 <waka5791@gmail.com>
2021-06-01 10:36:15 +0900

    Update timeInTitle.js

--- timeInTitle.js
+++ timeInTitle.js
@@ -1,25 +1,19 @@
-    const CheckIDName = 'DateInTitleAdded';
-    let _updateTitle = function () {
-        const _chkElem = document.getElementById(CheckIDName);
-        let _titleContent = document.title;
-        if (_chkElem === null) {
-            /*
-            let element = document.createElement('div');
-            element.id = CheckIDName;
-            element.style.display = 'none';
-            element.textContent = _titleContent;
-            document.body.appendChild(element);
-            */
-            let element = $('<div>').attr('id', CheckIDName).hide();
-            element.text(_titleContent);
-            $('body').append(element);
-        } else {
-            _titleContent = _chkElem.textContent;
-        }
-        let _now = new Date();
-        let _hour = _now.getHours();
-        let _min = _now.getMinutes();
-        let _sec = _now.getSeconds();
-        document.title = _hour + "時" + _min + "分" + _sec + "秒" + '【' + _titleContent + '】';
-    };
-    _updateTitle();
+const CheckIDName = 'DateInTitleAdded';
+let _updateTitle = function () {
+    const _chkElem = document.getElementById(CheckIDName);
+    let _titleContent = document.title;
+    if (_chkElem === null) {
+        let _div = $('<div>').attr('id', CheckIDName).hide();
+        _div.text(_titleContent);
+        $('body').append(_div);
+    } else {
+        _titleContent = _chkElem.textContent;
+    }
+    let _now = new Date();
+    let _hour = _now.getHours();
+    let _min = _now.getMinutes();
+    let _sec = _now.getSeconds();
+    let _timeStr = '${_hour}時${_min}分${_sec}秒';
+    document.title = _timeStr + '【' + _titleContent + '】';
+};
+_updateTitle();
