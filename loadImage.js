96de37774592dd6361e916c12643a1da11f388b8
waka5791 <waka5791@gmail.com>
2021-06-01 16:56:39 +0900

    Update loadImage.js

--- loadImage.js
+++ loadImage.js
@@ -1,16 +1,27 @@
-        jQuery(document).ready(function () {
-        const data = [
-            {
-                "id": 1,
-                "path": "https://github.com/waka5791/BookMarklets/blob/main/img/A.jpg?raw=true"
-            }
-        ];
-        let ulObj = $("#demo");
-        let     len = data.length;
+   
+jQuery(document).ready(function () {
+    const _data = [{
+        "id": 1,
+        "path": "https://github.com/waka5791/BookMarklets/blob/main/img/A.jpg?raw=true"
+    }];
+    let ulObj = $("#demo");
+    let _dataLen = _data.length;
 
-        for (var i = 0; i < len; i++) {
-            ulObj.append($("<li>").attr({
-                "id": data[i].id
-            }).text(data[i].path));
-        }
-        });
+    for (let _idx = 0; _idx < _dataLen; _idx++) {
+        _imgId = _data[_idx].id;
+        _imgPath = _data[_idx].path;
+        let _aTag = $("<a>",
+            {
+                "href": _imgPath,
+                "data-lightbox": "anonymous",
+                "data-title": _imgId
+            });
+        let _imgTag = $("<img>",
+            {
+                "src": _imgPath,
+                "alt": _imgId
+            });
+        _aTag.append(_imgTag);
+        ulObj.append(_aTag);
+    }
+});
