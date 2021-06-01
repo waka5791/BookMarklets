31232a623125520d4c25d7983f52f12bca1a3f54
waka5791 <waka5791@gmail.com>
2021-06-01 17:14:14 +0900

    Update loadImage.js

--- loadImage.js
+++ loadImage.js
@@ -1,19 +1,28 @@
-  jQuery(document).ready(function () {
+jQuery(document).ready(function () {
     const _baseDataUrl = "https://github.com/waka5791/BookMarklets/blob/main/img/";
-    const _data = [{
-        "id": 1,
-        "path": "A.jpg"
-    }];
+    const _data = [
+        {        
+            "id": 1,        
+            "path": "A.jpg",
+            "group": "y"
+        },
+        {
+            "id": 2,
+            "path": "B.jpg",
+            "group": "y"
+        }
+    ];
     let ulObj = $("#imageList");
     let _dataLen = _data.length;
 
     for (let _idx = 0; _idx < _dataLen; _idx++) {
         _imgId = _data[_idx].id;
         _imgPath = _baseDataUrl + _data[_idx].path + "?raw=true";
+        _imgGroup = _data[_idx].group;
         let _aTag = $("<a>",
             {
                 "href": _imgPath,
-                "data-lightbox": "anonymous",
+                "data-lightbox": _imgGroup,
                 "data-title": _imgId
             });
         let _imgTag = $("<img>",
