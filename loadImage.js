8b797bcb873b7805cabd015642fd1458d1fd4263
waka5791 <waka5791@gmail.com>
2021-06-03 00:22:44 +0900

    Update loadImage.js

--- loadImage.js
+++ loadImage.js
@@ -1,5 +1,6 @@
 jQuery(document).ready(function () {
-    const _baseDataUrl = "https://waka5791.github.io/RoughDrawings/img/";
+    //const _baseDataUrl = "https://waka5791.github.io/RoughDrawings/img/";
+    const _baseDataUrl = "https://raw.githubusercontent.com/waka5791/RoughDrawings/main/img/";
     $.getJSON("imageData.json", function (_data) {
         let _containerObj = $("#imageList");
         let _dataLen = _data.length;
@@ -22,19 +23,23 @@ jQuery(document).ready(function () {
         for (let _idx = 0; _idx < _dataLen; _idx++) {
             _imgId = _data[_idx].id;
             _imgPath = _baseDataUrl + _data[_idx].path;
-            _imgGroup = _data[_idx].group;
+            _caption = _data[_idx].caption;
+            if (_caption === undefined) {
+                _caption = "-";
+            }
             let _liTag = $('<span>');
             let _aTag = $("<a>",
                 {
                     "href": _imgPath,
-                    "caption": _imgId
+                    "caption": _caption
                 });
             let _imgTag = $("<img>",
                 {
                     "src": _imgPath,
                     "alt": _imgId,
                     "class": "xzoom-gallery",
-                    "xtitle": _imgId
+                    "xtitle": _imgId,
+                    "caption": _caption
                 });
 
             _liTag.append(_aTag);
@@ -42,20 +47,37 @@ jQuery(document).ready(function () {
             _galleryContainer.append(_liTag);
 
 
-            _aTag.on({
+            _imgTag.on({
                 'click': function () {
                     $('#imageCaption').text($(this).attr('caption'));
+                    $(this).addClass('grayImage');
                 }
             });
-            
+
         }
         _containerObj.append(_galleryContainer);
 
-        const _xzoomOption = { position: "right", fadeIn: true, hover: false };
+        const _xzoomOption = { position: "#zoomBox", fadeIn: true, hover: false };
         const instance = $(".xzoom, .xzoom-gallery").xzoom(_xzoomOption);
+
+
+        let _xxx = function () {
+            let xzoom = $(".xzoom:first").data('xzoom');
+            xzoom.closezoom();
+            let i, images = new Array();
+            let gallery = xzoom.gallery().ogallery;
+            let index = xzoom.gallery().index;
+            for (i in gallery) {
+                images[i] = { src: gallery[i] };
+            }
+            $.fancybox.open(images, { loop: false }, index);
+            event.preventDefault();
+        }
 
         //Integration with fancybox version 3 plugin
         $(".xzoom:first").bind('click', function (event) {
+            _xxx();
+            /*
             let xzoom = $(this).data('xzoom');
             xzoom.closezoom();
             let i, images = new Array();
@@ -66,6 +88,11 @@ jQuery(document).ready(function () {
             }
             $.fancybox.open(images, { loop: false }, index);
             event.preventDefault();
+            */
+        });
+
+        $(".fullScreenMode").bind('click', function (event) {
+            _xxx();
         });
 
         //Custom scale delta example
