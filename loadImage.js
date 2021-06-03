a53ad0a84e2c4e33b295fb6ec948490a2a561458
waka5791 <waka5791@gmail.com>
2021-06-03 19:17:18 +0900

    Update loadImage.js

--- loadImage.js
+++ loadImage.js
@@ -1,13 +1,22 @@
 jQuery(document).ready(function () {
-    //const _baseDataUrl = "https://waka5791.github.io/RoughDrawings/img/";
-    const _baseDataUrl = "https://raw.githubusercontent.com/waka5791/RoughDrawings/main/img/";
-    $.getJSON("imageData.json", function (_data) {
+    const _baseDataUrl = "https://waka5791.github.io/RoughDrawings/img/";
+    //const _baseDataUrl = "https://raw.githubusercontent.com/waka5791/RoughDrawings/main/img/";
+
+    let _data = null;
+    $.ajaxSetup({ async: false });
+    $.getJSON("imageData.json", function (jsonData) {
+        _data = JSON.parse(JSON.stringify(jsonData));
+    });
+    $.ajaxSetup({ async: true });
+
+    const CreateXzoomContainer = function (_data) {
         let _containerObj = $("#imageList");
         let _dataLen = _data.length;
         let _galleryContainer = $('<div>', { class: "xzoom-thmubs" });
 
         if (_dataLen > 0) {
             _imgId = _data[0].id;
+
             _imgPath = _baseDataUrl + _data[0].path;
             _imgGroup = _data[0].group;
             let _imgTag = $("<img>",
@@ -21,24 +30,29 @@ jQuery(document).ready(function () {
         }
 
         for (let _idx = 0; _idx < _dataLen; _idx++) {
-            _imgId = _data[_idx].id;
-            _imgPath = _baseDataUrl + _data[_idx].path;
-            _caption = _data[_idx].caption;
+            let _imgId = _data[_idx].id;
+            let _dataPath = _data[_idx].path;
+            let _imgPath = _baseDataUrl + _dataPath;
+            if(_dataPath.match(/^http/) != null){
+                _imgPath = _dataPath;
+            }
+            let _caption = _data[_idx].caption;
             if (_caption === undefined) {
                 _caption = "-";
             }
-            let _liTag = $('<span>');
+            let _liTag = $('<div>', { class: "singleImageContainer" });
             let _aTag = $("<a>",
                 {
                     "href": _imgPath,
-                    "caption": _caption
+                    "data-fancybox": "images",
+                    "data-caption": _caption
                 });
             let _imgTag = $("<img>",
                 {
                     "src": _imgPath,
-                    "alt": _imgId,
+
                     "class": "xzoom-gallery",
-                    "xtitle": _imgId,
+                    "xtitle": _caption,
                     "caption": _caption
                 });
 
@@ -46,29 +60,33 @@ jQuery(document).ready(function () {
             _aTag.append(_imgTag);
             _galleryContainer.append(_liTag);
 
+            let _cTag = $('<div>', { class: "innerCaption" });
+            _cTag.text(_idx + 1);
+            _liTag.append(_cTag);
 
             _imgTag.on({
                 'click': function () {
-                    $('#imageCaption').text($(this).attr('caption'));
+                    $('#imageCaption').html($(this).attr('caption'));
                     $(this).addClass('grayImage');
                 }
             });
 
         }
         _containerObj.append(_galleryContainer);
+    }
 
-        const _xzoomOption = { position: "#zoomBox", fadeIn: true, hover: false };
+    const Enhancer = function () {
+        const _xzoomOption = { position: "#zoomBox", lensShape: "box", fadeIn: true, hover: false };
         const instance = $(".xzoom, .xzoom-gallery").xzoom(_xzoomOption);
-
-
-        let _xxx = function () {
+        let _kickFancyBox = function (event) {
             let xzoom = $(".xzoom:first").data('xzoom');
             xzoom.closezoom();
             let i, images = new Array();
             let gallery = xzoom.gallery().ogallery;
             let index = xzoom.gallery().index;
             for (i in gallery) {
-                images[i] = { src: gallery[i] };
+                let _caption = _data[i].caption;
+                images[i] = { src: gallery[i], caption: _caption };
             }
             $.fancybox.open(images, { loop: false }, index);
             event.preventDefault();
@@ -76,23 +94,11 @@ jQuery(document).ready(function () {
 
         //Integration with fancybox version 3 plugin
         $(".xzoom:first").bind('click', function (event) {
-            _xxx();
-            /*
-            let xzoom = $(this).data('xzoom');
-            xzoom.closezoom();
-            let i, images = new Array();
-            let gallery = xzoom.gallery().ogallery;
-            let index = xzoom.gallery().index;
-            for (i in gallery) {
-                images[i] = { src: gallery[i] };
-            }
-            $.fancybox.open(images, { loop: false }, index);
-            event.preventDefault();
-            */
+            // _kickFancyBox(event);
         });
 
-        $(".fullScreenMode").bind('click', function (event) {
-            _xxx();
+        $("#fullScreenMode").bind('click', function (event) {
+            _kickFancyBox(event);
         });
 
         //Custom scale delta example
@@ -111,6 +117,62 @@ jQuery(document).ready(function () {
         }
 
 
-    });
+        let _isZoom = true;
+        instance.eventopen = function (element) {
+            if (_isZoom) {
+                element.bind('mouseenter', instance.openzoom);
+            } else {
+                instance.closezoom();
+                element.unbind('mouseenter');
+            }
+        }
+        instance.eventmove = function (element) {
+            if (_isZoom) {
+                element.bind('mousemove', instance.movezoom);
+            } else {
+                instance.closezoom();
+                element.unbind('mousemove');
+            }
+        }
+        $('#zoomOnOff').on({
+            'click': function (event) {
+                _isZoom = !_isZoom;
+                if (_isZoom) {
+                    $(".xzoom:first").trigger('mouseenter');
+                    instance.closezoom();
+                } else {
 
+                    //instance.eventdefault('eventmove');
+                }
+                _isZoom ? $('#badge2').html("&#10003;") : $('#badge2').html("&#9633;");
+            }
+        });
+
+
+        $('#zoomBoxOnOff').on({
+            'click': function (event) {
+                $('#zoomBox').visibleToggle();
+
+                if ($('#zoomBox').css("visibility") == "visible") {
+                    $('#badge1').html("&#10003;");
+                } else {
+                    $('#badge1').html("&#9633;");
+                }
+
+            }
+        });
+
+    }
+
+    CreateXzoomContainer(_data);
+    Enhancer();
+    $('#zoomBox').inviewChecker();
+    $(window).scroll(function(){
+        if($('#zoomBox').hasClass('_item_in_all')){
+            //$('#debugConsole').text('inner ');
+        }else{
+            //$('#debugConsole').text('outer ');
+            $('#zoomBox').css({top:$('#previewBox').offset().top});
+        }
+    });
 });
