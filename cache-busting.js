4e10be656400de6889b42307c2c221a1f9082bd8
waka5791 <waka5791@gmail.com>
2021-07-08 17:01:41 +0900

    Update cache-busting.js

--- cache-busting.js
+++ cache-busting.js
@@ -1,8 +1,26 @@
 jQuery(document).ready(function () {
-    $.ajaxSetup({
-        cache: true
-    });
-    const _ts = new Date().getTime()
+
+    const _ua = navigator.userAgent.toLowerCase();
+    const _isIE = (_ua.indexOf('trident') != -1 || _ua.indexOf('msie') != -1);
+    if (_isIE) {
+        const _parent = document.getElementById('mainContainer');
+        while (_parent.firstChild) {
+            _parent.removeChild(_parent.firstChild);
+        }
+        const _pJa = document.createElement("center");
+        _pJa.innerText = 'Internet Explorerには対応していません。他の最新ブラウザをご利用ください。';
+        _parent.appendChild(_pJa);
+        _parent.appendChild(document.createElement("hr"));
+        const _pEn = document.createElement("center");
+        _pEn.innerText = 'Unsupported.Please use a modern browser other than this Internet Explorer.';
+        _parent.appendChild(_pEn);
+
+        _parent.style.display = "block";
+        return;
+    }
+    $.ajaxSetup({ cache: true });
+
+    const _ts = new Date().getTime();
     let _l = $('<link>').attr({ 'rel': 'stylesheet', 'href': 'main.css?' + _ts });
     $('head').append(_l);
     let _s1 = $('<script>').attr({ 'type': 'text/javascript', 'src': 'jquery-my-util.js?' + _ts });
